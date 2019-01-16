b4030df22615d83d39c474f137aeaad247f9dc2d
Axel Köhler <koehler.ax3l@gmail.com>
2019-01-16 15:53:44 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -n "${TO_FILE}" ]; then
+  file_name=$(date -u +"%Y-%m-%dT%H:%M:%SZ").txt
+  traceroute ${1} >> /data/$file_name
+
+  echo $file_name >> /data/full-log.txt
+  cat /data/$file_name >> /data/full-log.txt
+  echo $file_name >> /data/max-values.txt
+  cat /data/$file_name | grep -v "traceroute" | awk -F' ' '{print $4} {print $6} {print $8}' | sort -nr | head -n1 >> /data/max-values.txt
+else
+  traceroute ${1}
+fi
